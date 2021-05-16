3996922b493f7ed96a514963c1df2fda843458d2
dikr <dikr@rmt.com.au>
2021-05-16 18:08:10 +0800

    add compression to app

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const compression = require('compression');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
@@ -15,27 +16,20 @@ const auth = require('./routes/auth');
 
 const app = express();
 
+app.use(compression());
 app.use(express.json());
-
 app.use(cookieParser());
-
 app.use(mongoSanitize());
-
 app.use(helmet());
-
 app.use(hpp());
-
 app.use(cors(corsOptions));
-
 app.use('/api/v1/auth', auth);
-
 app.use(errorHandler);
 
-app.listen(
-  port,
-  log.info(`Server running in ${port}`),
-);
+app.listen(port, log.info(`Server running in ${port}`));
 
 process.on('unhandledRejection', (err) => {
-  log.error(`Error: ${err.message}`);
+  log.error(err.message);
 });
+
+module.exports = app;
